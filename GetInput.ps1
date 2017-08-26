7f020ba84b5c73704ddde0d5f9c4e6830a689a13
Jacob Kelley <exaybachay.ak@gmail.com>
2017-08-25 22:17:19 -0800

    Update GetInput.ps1

--- GetInput.ps1
+++ GetInput.ps1
@@ -1,1 +1,5 @@
 $userinput = Read-Host -prompt "What would you like to do?"
+
+#Read in an array
+#https://copysave.wordpress.com/2010/07/02/read-host-values-into-an-array/
+$hosts = (Read-host -Prompt 'Enter a list of hosts:').split(',') | ForEach-Object {$_.trim()}
